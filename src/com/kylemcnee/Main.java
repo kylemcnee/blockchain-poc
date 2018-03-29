beb5e13a5a21b03c40c6ac0ea59ba7bc1d940281
kmcnee <kyle.mcnee@gmail.com>
2018-03-29 17:05:11 -0400

    completed Main.java

--- src/com/kylemcnee/Main.java
+++ src/com/kylemcnee/Main.java
@@ -4,5 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String[] genesisTransactions = {"Rob sent Kyle 1.093 Dogecoin","Kyle sent Rob .082 Monero"};
+        Block genesisBlock = new Block(genesisTransactions,0);
+
+        String[] block2Transactions = {"Elon sent 50 bitcoin to Satoshi", "Satoshi sent Elon 10 bitcoin"};
+        Block block2 = new Block(block2Transactions, genesisBlock.getCurrentHash());
+
+        System.out.println("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
+        System.out.println("This is the hash of the Genesis Block,");
+        System.out.println("the very first block in our Blockchain:");
+        System.out.println(genesisBlock.getCurrentHash());
+
+        System.out.println("Here is the hash of our second block:");
+        System.out.println(block2.getCurrentHash());
+
+        System.out.println("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
+
+        System.out.println("Checking the hash allows us to be certain that");
+        System.out.println("the data in each block has not been tampered with.");
+        System.out.println("Try writing down the hash of both blocks,");
+        System.out.println("then run the program again and see how the Hashes are affected.");
     }
 }
